cebbc5af744b3e68f33a2942564bb347dda73bdc
Swapnil Opash <sra.opash@gmail.com>
2024-03-15 17:07:17 +0530

    fixed issue

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -91,9 +91,9 @@ User.login = function (email, Id, result) {
             {
               id: res[0].profileId,
               username: res[0].Username,
-              active: res[0].IsActive
+              active: res[0].IsActive,
             },
-            '5d'
+            "5d"
           );
 
           return result(null, {
@@ -287,11 +287,11 @@ User.adminLogin = function (email, result) {
           {
             id: res[0].profileId,
             username: res[0].Username,
-            active: res[0].IsActive
+            active: res[0].IsActive,
           },
-          '5d'
+          "5d"
         );
-        
+
         return result(null, {
           userId: user.Id,
           user: user,
@@ -453,10 +453,10 @@ User.verification = function (token, result) {
     try {
       const updateQuery = await executeQuery(
         "UPDATE users SET IsActive ='Y' WHERE Id = ?",
-        [decoded.userId]
+        [decoded.user.userId]
       );
       const fetchUser = await executeQuery("select * from users where Id = ?", [
-        decoded.userId,
+        decoded.user.userId,
       ]);
       console.log("fetchUser", updateQuery, fetchUser);
       return result(null, fetchUser[0]);
