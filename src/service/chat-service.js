b3b700aa9443d7feae02442b1d88e8d7eb8a9e23
hirendanecha <hirendanecha@gmail.com>
2024-12-17 15:56:45 +0530

    applied changes

--- src/service/chat-service.js
+++ src/service/chat-service.js
@@ -1,5 +1,9 @@
 const { executeQuery } = require("../helpers/utils");
-const { notificationMailOnInvite } = require("../helpers/utils");
+const {
+  notificationMailOnInvite,
+  notificationMail,
+} = require("../helpers/utils");
+
 const { getPagination, getCount, getPaginationData } = require("../helpers/fn");
 const moment = require("moment");
 
@@ -90,7 +94,6 @@ exports.readGroupMessage = async function (data) {
 exports.resendRoom = async function (data) {
   return await resendRoom(data);
 };
-
 exports.userStatus = async function (id) {
   return await userStatus(id);
 };
@@ -101,7 +104,9 @@ exports.changeUserStatus = async function (data) {
 exports.getMessages = async function (data) {
   return await getMessages(data);
 };
-
+exports.getRoomByProfileId = async function (data) {
+  return await getRoomByProfileId(data);
+};
 exports.endCall = async function (data) {
   return await endCall(data);
 };
@@ -112,6 +117,10 @@ exports.checkCall = async function (data) {
 
 exports.suspendUser = function (data) {
   return suspendUser(data);
+};
+
+exports.sendNotificationEmail = async function (data) {
+  return await sendNotificationEmail(data);
 };
 
 const getChatList = async function (params) {
@@ -125,6 +134,7 @@ const getChatList = async function (params) {
                   r.updatedDate,
                   r.createdDate,
                   r.isDeleted,
+                  r.sentBy,
                   p.ID AS profileId,
                   p.Username,
                   p.FirstName,
@@ -142,7 +152,7 @@ LEFT JOIN
 WHERE
     (r.profileId1 = ? OR r.profileId2 = ?) AND r.isDeleted = 'N'
 GROUP BY
-    r.id, r.profileId1, r.isAccepted,r.updatedDate, p.ID, p.Username, p.FirstName, p.LastName, p.ProfilePicName
+    r.id, r.profileId1, r.isAccepted,r.updatedDate,r.sentBy, p.ID, p.Username, p.FirstName, p.LastName, p.ProfilePicName
 ORDER BY
 r.updatedDate desc;`;
     const values = [params.profileId, params.profileId];
@@ -255,31 +265,92 @@ const sendMessage = async function (params) {
         if (data.roomId) {
           const date = new Date();
           const query =
-            "update chatRooms set lastMessageText = ?,updatedDate = ? where id = ?";
-          const values = [data.messageText, date, data.roomId];
+            "update chatRooms set lastMessageText = ?,updatedDate = ?,sentBy = ? where id = ?";
+          const values = [data.messageText, date, data?.sentBy, data.roomId];
           const updatedRoom = await executeQuery(query, values);
-          const notification = await createNotification({
-            notificationToProfileId: params?.profileId,
-            roomId: data?.roomId,
-            notificationByProfileId: data?.sentBy,
-            actionType: "M",
-            msg: "sent you a message",
-          });
-          return { newMessage, notification };
+          if (params.messageType === "C" || params.messageType === "D") {
+            const notification = await createNotification({
+              notificationToProfileId: params?.profileId,
+              roomId: data?.roomId,
+              notificationByProfileId: data?.sentBy,
+              actionType: "M",
+              msg:
+                params.messageType === "C"
+                  ? "You have missed call from"
+                  : "has declined your call",
+              messageType: params.messageType,
+            });
+            return { newMessage, notification };
+          } else {
+            const notification = await createNotification({
+              notificationToProfileId: params?.profileId,
+              roomId: data?.roomId,
+              notificationByProfileId: data?.sentBy,
+              actionType: "M",
+              msg: "sent you a message",
+            });
+            return { newMessage, notification };
+          }
         }
         if (data.groupId) {
           const date = new Date();
           const query =
-            "update chatGroups set lastMessageText = ?,updatedDate = ? where id = ?";
-          const values = [data.messageText, date, data.groupId];
+            "update chatGroups set lastMessageText = ?,updatedDate = ?,sentBy = ? where id = ?";
+          const values = [data.messageText, date, data?.sentBy, data.groupId];
           const updatedGroup = await executeQuery(query, values);
-          const notification = await createNotification({
-            // notificationToProfileId: params.profileId,
-            groupId: data?.groupId,
-            notificationByProfileId: data?.sentBy,
-            actionType: "M",
-            msg: "sent you a message in group",
-          });
+          let notification = {};
+          if (params.messageType === "C") {
+            notification = await createNotification({
+              notificationToProfileId: params?.profileId,
+              roomId: data?.roomId,
+              notificationByProfileId: data?.sentBy,
+              actionType: "M",
+              msg: "You have missed call from",
+              messageType: params.messageType,
+            });
+          } else {
+            notification = await createNotification({
+              // notificationToProfileId: params.profileId,
+              groupId: data?.groupId,
+              notificationByProfileId: data?.sentBy,
+              actionType: "M",
+              msg: "sent you a message in group",
+            });
+          }
+          if (params?.tags?.length > 0) {
+            const notifications = [];
+            for (const key in params?.tags) {
+              if (Object.hasOwnProperty.call(params?.tags, key)) {
+                const tag = params?.tags[key];
+
+                // const notification = await createNotification({
+                //   notificationToProfileId: tag?.id,
+                //   groupId: data?.groupId,
+                //   notificationByProfileId: data?.sentBy,
+                //   actionType: "T",
+                //   msg: "tagged you in message",
+                // });
+                const findUser = `select u.Email,p.FirstName,p.LastName,p.Username from users as u left join profile as p on p.UserID = u.Id where p.messageNotificationEmail = 'Y' and p.ID = ?`;
+                const values = [tag?.id];
+                const userData = await executeQuery(findUser, values);
+                if (userData?.length) {
+                  const senderData = await getGroup({ groupId: data?.groupId });
+                  // notifications.push(notification);
+                  if (tag?.id) {
+                    const userDetails = {
+                      email: userData[0].Email,
+                      userName: userData[0].Username,
+                      senderUsername: senderData.groupName,
+                      ProfilePicName: senderData.profileImage,
+                      type: "message",
+                      groupId: senderData?.id,
+                    };
+                    await notificationMail(userDetails);
+                  }
+                }
+              }
+            }
+          }
           return { newMessage, notification };
         }
       }
@@ -355,12 +426,19 @@ const createNotification = async function (params) {
       notificationByProfileId,
       actionType,
       msg,
+      messageType = "",
     } = params;
     const query =
       "SELECT ID,ProfilePicName, Username, FirstName,LastName from profile where ID = ?";
     const values = [notificationByProfileId];
     const userData = await executeQuery(query, values);
-    let desc = `${userData[0]?.Username || userData[0]?.FirstName} ${msg}`;
+
+    let desc;
+    if (messageType === "C") {
+      desc = `${msg} ${userData[0]?.Username || userData[0]?.FirstName}`;
+    } else {
+      desc = `${userData[0]?.Username || userData[0]?.FirstName} ${msg}`;
+    }
 
     const data = {
       notificationToProfileId: notificationToProfileId || null,
@@ -370,6 +448,7 @@ const createNotification = async function (params) {
       actionType: actionType,
       notificationDesc: desc,
     };
+    console.log("New-notification", data);
     if (data.notificationByProfileId !== data.notificationToProfileId) {
       const query1 = "insert into notifications set ?";
       const values1 = [data];
@@ -473,15 +552,15 @@ const editMessage = async function (params) {
     if (data.roomId) {
       const date = new Date();
       const query =
-        "update chatRooms set lastMessageText = ?,updatedDate = ? where id = ?";
-      const values = [data.messageText, date, data.roomId];
+        "update chatRooms set lastMessageText = ?,updatedDate = ?,sentBy = ? where id = ?";
+      const values = [data.messageText, date, data?.sentBy, data.roomId];
       const updatedRoom = await executeQuery(query, values);
     }
     if (data.groupId) {
       const date = new Date();
       const query =
-        "update chatGroups set lastMessageText = ?,updatedDate = ? where id = ?";
-      const values = [data.messageText, date, data.groupId];
+        "update chatGroups set lastMessageText = ?,updatedDate = ?,sentBy = ? where id = ?";
+      const values = [data.messageText, date, data?.sentBy, data.groupId];
       const updatedGroup = await executeQuery(query, values);
     }
     const query1 = "select * from messages where id = ?";
@@ -522,22 +601,28 @@ const deleteMessage = async function (params) {
         const values1 = [
           messageList?.messageText || null,
           messageList?.createdDate,
+          messageList?.sentBy,
           data.roomId,
         ];
         const updatedRoom = await executeQuery(query1, values1);
         console.log("updateRoom->", updatedRoom);
         data.isDeleted = true;
         return data;
+        // if (messageList) {
+        // }
       }
       if (data?.groupId) {
         const query =
           "select * from messages where groupId = ? order by createdDate desc limit 1";
         const values = data.groupId;
         [messageList] = await executeQuery(query, values);
-        const query1 = `update chatGroups set lastMessageText = ?,updatedDate = ? where id = ?`;
+        // if (messageList) {
+        // }
+        const query1 = `update chatGroups set lastMessageText = ?,updatedDate = ?,sentBy = ? where id = ?`;
         const values1 = [
           messageList?.messageText || null,
           messageList?.createdDate,
+          messageList?.sentBy,
           data.groupId,
         ];
         const updatedRoom = await executeQuery(query1, values1);
@@ -627,21 +712,24 @@ const startCall = async function (params) {
     if (params) {
       const notificationToProfileId = params?.notificationToProfileId ?? null;
       const groupId = params?.groupId ?? null;
-      const query = `select * from calls_logs where (profileId = '${notificationToProfileId}' or groupId = '${groupId}') and isOnCall = 'Y' and endDate is null`;
+      const query = `select * from calls_logs where (roomId = '${params?.roomId}' or groupId = '${groupId}') and isOnCall = 'Y' and endDate is null`;
       const [callLogs] = await executeQuery(query);
+      console.log("callLogs", callLogs);
+
       const callLogsData = {
-        profileId: params?.notificationByProfileId,
+        // profileId: params?.notificationByProfileId,
         isOnCall: "Y",
         roomId: params?.roomId || null,
         groupId: params?.groupId || null,
         callLink: params?.link || null,
+        members: callLogs?.members || 1,
       };
-      if (callLogsData) {
+      if (!callLogs) {
         const query = `insert into calls_logs set ?`;
         const values = [callLogsData];
         await executeQuery(query, values);
       }
-      console.log("callLogs", callLogs);
+
       if (params?.roomId) {
         const data = {
           notificationToProfileId: params?.notificationToProfileId || null,
@@ -678,6 +766,7 @@ const startCall = async function (params) {
         const group = await getGroup({ groupId: data.groupId });
         notification["ProfilePicName"] = group?.profileImage;
         notification["groupName"] = group?.groupName;
+        notification["members"] = callLogsData?.members;
         if (callLogs?.isOnCall === "Y") {
           notification["isOnCall"] = callLogs?.isOnCall;
         } else {
@@ -694,14 +783,15 @@ const startCall = async function (params) {
 const declineCall = async function (params) {
   try {
     if (params) {
-      let query = "";
-      if (params?.roomId) {
-        query = `update calls_logs set endDate = now(), isOnCall = 'N' where roomId = ${params.roomId} and isOnCall = 'Y' and endDate is null`;
-      } else {
-        query = `update calls_logs set endDate = now(), isOnCall = 'N' where groupId = ${params.groupId} and profileId = ${params.notificationByProfileId} and isOnCall = 'Y' and endDate is null`;
+      if (params) {
+        let query = "";
+        query = `update calls_logs set endDate = now(), isOnCall = 'N',members = ${params?.members} where (roomId = ${params?.roomId} or groupId = ${params?.groupId}) and isOnCall = 'Y' and members >=  1 and endDate is null`;
+        await executeQuery(query);
       }
+      // else {
+      //   query = `update calls_logs set endDate = now(), isOnCall = 'N' where groupId = ${params.groupId} and isOnCall = 'Y' and endDate is null`;
+      // }
       // const query = `update calls_logs set endDate = now(), isOnCall = 'N' where (roomId = ${params.roomId} or groupId = ${params.groupId}) and isOnCall = 'Y'`;
-      await executeQuery(query);
       const data = {
         notificationToProfileId: params?.notificationToProfileId || null,
         roomId: params?.roomId,
@@ -711,7 +801,7 @@ const declineCall = async function (params) {
         msg: params.message || "call decline...",
       };
       // const notification = await createNotification(data);
-      return params?.roomId ? data : {};
+      return params?.members === 0 ? data : {};
     }
   } catch (error) {
     return error;
@@ -721,17 +811,30 @@ const declineCall = async function (params) {
 const pickUpCall = async function (params) {
   try {
     if (params) {
+      // const notificationToProfileId = params?.notificationToProfileId ?? null;
+      const groupId = params?.groupId ?? null;
+      const query = `select * from calls_logs where (roomId = '${params?.roomId}' or groupId = '${groupId}') and isOnCall = 'Y' and endDate is null`;
+      const [existingCall] = await executeQuery(query);
+
       const callLogs = {
-        profileId: params?.notificationByProfileId,
+        // profileId: params?.notificationByProfileId,
         isOnCall: "Y",
         roomId: params?.roomId || null,
         groupId: params?.groupId || null,
         callLink: params?.link || null,
+        members: existingCall.members + 1,
       };
-      if (callLogs) {
-        const query = `insert into calls_logs set ?`;
+      console.log("callLogs", existingCall);
+      if (existingCall) {
+        const query = `update calls_logs set members = ${callLogs?.members}, callLink = '${callLogs?.callLink}' where id = ${existingCall?.id}`;
         const values = [callLogs];
         await executeQuery(query, values);
+      } else {
+        if (callLogs) {
+          const query = `insert into calls_logs set ?`;
+          const values = [callLogs];
+          await executeQuery(query, values);
+        }
       }
       const data = {
         notificationToProfileId: params?.notificationToProfileId || null,
@@ -924,7 +1027,6 @@ const getUserDetails = async function (id) {
 const switchChat = async function (params) {
   const query = `update groupMembers set switchDate = '${params.date}' where groupId = ${params.groupId} and profileId = ${params.profileId}`;
   const data = await executeQuery(query);
-  console.log(data);
   return data;
 };
 
@@ -1059,32 +1161,120 @@ const getReadUser = async function (msg) {
   }
 };
 
-const endCall = async function (data) {
+const getRoomByProfileId = async function (data) {
   try {
-    const query = `update calls_logs set isOnCall = 'N', endDate = NOW() where profileId = ${data?.profileId} and (groupId = ${data?.groupId} or roomId = ${data?.roomId}) and endDate is null`;
-    const callData = await executeQuery(query);
-    return callData;
+    const query = `select 
+                  r.id AS roomId,
+                  r.profileId1 AS createdBy,
+                  r.isAccepted,
+                  r.lastMessageText,
+                  r.updatedDate,
+                  r.createdDate,
+                  r.isDeleted,
+                  p.ID AS profileId,
+                  p.Username,
+                  p.FirstName,
+                  p.LastName,
+                  p.ProfilePicName
+    from chatRooms as r left join
+    profile as p on p.ID = ${data.profileId2} where r.isDeleted = 'N' and (r.profileId1 = ${data.profileId1} and r.profileId2 = ${data.profileId2} OR r.profileId1 = ${data.profileId2} and r.profileId2 = ${data.profileId1})`;
+    const rooms = await executeQuery(query);
+    return rooms;
   } catch (error) {
     return error;
+  }
+};
+
+const endCall = async function (data) {
+  try {
+    console.log("data==?", data);
+    // const query = `
+    //   UPDATE calls_logs
+    //   SET
+    //     isOnCall = ?,
+    //     ${data.members > 0  ? 'endDate = NOW(),' :''}
+    //     members = ?
+    //   WHERE
+    //     (groupId = ? OR roomId = ?)
+    //     AND endDate IS NULL
+    // `;
+
+    let query = `
+    UPDATE calls_logs
+    SET 
+      isOnCall = ?
+  `;
+    if (data.members === 0) {
+      query += `,
+      endDate = NOW()
+    `;
+    }
+    query += `,
+      members = ?
+    WHERE 
+      (groupId = ? OR roomId = ?)
+      AND callLink = ?
+      AND endDate IS NULL
+  `;
+    const params = [
+      data.isOnCall,
+      data.members,
+      data.groupId || null,
+      data.roomId || null,
+      data.callLink,
+    ];
+    const callData = await executeQuery(query, params);
+    console.log("endCallData====>", callData);
+
+    return callData;
+  } catch (error) {
+    console.error("Error in endCallData:", error); // Log the error for debugging
+    throw error; // Throw the error to ensure it can be handled by the caller
   }
 };
 
 const checkCall = async function (data) {
   try {
-    const query = `select * from calls_logs where profileId = ${data?.profileId} and isOnCall = 'Y' and endDate is null`;
+    console.log("params==>", data);
+    const query = `select * from calls_logs where (groupId = ${data?.groupId} or roomId = ${data?.roomId}) and isOnCall = 'Y' and members >=  1 and endDate is null`;
     const [callData] = await executeQuery(query);
+    console.log("callData", callData);
+
     return callData;
   } catch (error) {
     return error;
   }
 };
-
 const suspendUser = async function (params) {
   try {
     const query = "UPDATE users SET IsSuspended = ? WHERE Id= ?";
     const values = [params.isSuspended, params.id];
     const user = await executeQuery(query, values);
     return user;
+  } catch (error) {
+    return error;
+  }
+};
+
+const sendNotificationEmail = async function (data) {
+  try {
+    const findUser = `select u.Email,p.FirstName,p.LastName,p.Username from users as u left join profile as p on p.UserID = u.Id where p.messageNotificationEmail = 'Y' and p.ID = ?`;
+    const values = [data?.profileId];
+    const userData = await executeQuery(findUser, values);
+    if (userData?.length) {
+      const senderData = await getRoom(data?.roomId);
+      console.log(senderData);
+      const userDetails = {
+        email: userData[0].Email,
+        userName: userData[0].Username,
+        senderUsername: senderData.Username,
+        type: "message",
+        roomId: senderData?.roomId,
+      };
+      await notificationMail(userDetails);
+    } else {
+      return true;
+    }
   } catch (error) {
     return error;
   }
